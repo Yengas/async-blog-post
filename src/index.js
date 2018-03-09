52b3a6028229526f9860c2ee5d8217e968493102
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-03-09 21:53:38 +0300

    read file into an array and do request for each of them.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+// Dosya okumak için node.js kütüphanemizi alalım.
+const fs = require('fs');
 // Sync olarak request yapmamızı sağlayacak, kütüphane.
 const request = require('sync-request');
-// bilgilerini almak istediğimiz filmin id'si.
-const movieID = '2baf70d1-42bb-4437-b551-e5fed5a87abe';
+// Tüm film id'lerini, dosyamızı okuyarak ve '\n' karakterinden split yaparak, bir diziye atıyalım.
+const movieIDs = fs.readFileSync('../data/ghibli_movies.txt').toString().split('\n');
 
-// API'ye yaptığımız istek
-const result = request('GET', `https://ghibliapi.herokuapp.com/films/${movieID}`);
-// API'den gelen string cevabı, javascript objesini çevirmek için JSON parse işlemi
-const body = JSON.parse(result.body.toString());
-// obje içerisindeki title ve description değerlerinin, aynı isimle değişkene atanması.
-const { title, description } = body;
+for(let movieID of movieIDs){
+  // API'ye yaptığımız istek
+  const result = request('GET', `https://ghibliapi.herokuapp.com/films/${movieID}`);
+  // API'den gelen string cevabı, javascript objesini çevirmek için JSON parse işlemi
+  const body = JSON.parse(result.body.toString());
+  // obje içerisindeki title ve description değerlerinin, aynı isimle değişkene atanması.
+  const { title, description } = body;
 
-// Artık sonucu ekrana yazdırabiliriz, umarız şimdiye kadar hata olmamıştır...
-console.log(`Film Adı: ${title}`);
-console.log(`Filmin açıklaması: ${description}`);
+  // Artık sonucu ekrana yazdırabiliriz, umarız şimdiye kadar hata olmamıştır...
+  console.log(`Film Adı: ${title}`);
+  console.log(`Filmin açıklaması: ${description}`);
+}
